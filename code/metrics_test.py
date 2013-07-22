4f960fedebabcbd3bae16e3238df3a1bd22a3dbf
Laura Dietz <dietz@cs.umass.edu>
2013-07-22 18:30:41 -0400

    clean test case delete obsolete files

--- code/metrics_test.py
+++ code/metrics_test.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 18 22:39:48 2013
-
-@author: dietz
+For automated test of metrics with respectg other evaluation package (galago's trec_eval)
 """
 
 from metrics import *
@@ -14,22 +12,12 @@ metrics = {
 }
 
 
-input = ','.join(sys.argv[1:])
+commandinput = ','.join(sys.argv[1:])
 
-arr = [True if str.lower()=='true' else False for str in input.split(',')]
-gt = sum(1 if str.lower()=='true' else 0 for str in input.split(','))
+arr = [True if split.lower()=='true' else False for split in commandinput.split(',')]
+gt = sum(1 if split.lower()=='true' else 0 for split in commandinput.split(','))
 gf = len(arr) - gt
 
 
 for metricname, metric in metrics.items():
     print metricname,metric(np.array(arr), gt, gt)
-
-
-#print 0.0, metric(np.array([False]), 0,1)
-#print 0.0, metric(np.array([]), 1,0)
-#print 1.0, metric(np.array([True,True,False,False]), 2, 2)
-#print 1./2, metric(np.array([False,True,True,False]), 2, 2)
-#print 1./2, metric(np.array([True,False,False,True]), 2, 2)
-#print 2./3, metric(np.array([True,False,False,True,False]), 2, 3)
-#print 1./2, metric(np.array([True,True,True,True]), 4, 0)
-#print 2./3, metric(np.array([True,True,False,True]), 2, 0)
