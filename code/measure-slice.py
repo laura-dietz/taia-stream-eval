6fd362ce0a848614f82a58ea90ef6242db0b81c7
Laura Dietz <dietz@cs.umass.edu>
2013-07-22 18:27:21 -0400

    clean up measure-slice, also add output for trec_eval as a command line option

--- code/measure-slice.py
+++ code/measure-slice.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# example usage
-# python real.py --intervalType week --judgmentLevel 1 -f ~/kba-evaluation/taia/data/umass-runs/UMass_CIIR-PC_RM20_1500.gz
+"""
+Measure performance for a run file, an intervalType (day,week, all), and a judgmentLevel (1 or 2).
 
+Creates the output next to the input file, appending "-eval-week.tsv" (for weeks)
+
+ example usage
+ python measure-slice.py --intervalType week --judgmentLevel 1 -f ~/kba-evaluation/taia/data/umass-runs/UMass_CIIR-PC_RM20_1500.gz
+"""
 
 import os.path
 import numpy as np
 from numpy.lib import recfunctions
 import gzip
 from metrics import *
-import sys
 import string
 from utils import *
 from truthutil import *
@@ -18,28 +22,26 @@ from argparse import ArgumentParser
 
         
 DEBUG=False        
-DUMP_TREC_EVAL=False        
-        
+
 metrics = {
 'nDCG@R': ndcgREval,'Prec@R': precREval, 'correctedAUC':normalizeAUC, 'MAP':mapEval, 'extendedAUC':normalizeExtendedAUC}
 
-#intervalType = 'all'
-#judgmentLevel = 1
 
 runFile = '~/kba-evaluation/taia/data/umass-runs/UMass_CIIR-PC_RM20_1500.gz'        
-    #runFile = '/media/dietz/bob/taia/all-runs/input.UMass_CIIR-PC_RM20_1500.gz'
 
 
 
 parser = ArgumentParser()
-parser.add_argument('--intervalType', default='all')
+parser.add_argument('--intervalType', help='slice segmentation: day, week, or all', default='all')
 parser.add_argument('--judgmentLevel', type=int, help='Judgement level', default=1)
+parser.add_argument('--trec_eval', action='store_true', help='Also create output for evaluation with trec_eval', default=False)
 parser.add_argument('-f', '--runFile', metavar='FILE', default=runFile)
 args = parser.parse_args()
 
 judgmentLevel = args.judgmentLevel
 intervalType = args.intervalType
 runFile = args.runFile
+DUMP_TREC_EVAL=args.trec_eval  # also create output for trec_eval.
 
 
         
@@ -49,7 +51,6 @@ entry_dtype = np.dtype([('runname','50a'),('docid', '50a'), ('query', '50a'),
 judg_dtype = np.dtype([('docid', '50a'), ('query', '50a'),
                         ('label', 'd4')])
 eval_dtype = np.dtype([('team','50a'),('runname','50a'),('query','50a'),('intervalLow','d4'),('intervalUp','d4'),('unjudged','50a'),('judgmentLevel','d4'),('metric','50a'),('value','f4')])                        
-#trec_eval_dtype = np.dtype([('query','50a'),('runname','50a'),('docid','50a'),('rank','d4'),('score','f4'),('desc','50a')])                        
 
 def read_predictions(fname):
     a = np.genfromtxt(fname, dtype=entry_dtype,comments='#', usecols=[1,2,3,4])
@@ -115,32 +116,14 @@ fullEntityList = [
     'William_D._Cohan',
     'William_H._Gates,_Sr',
     ]
-#entityListFromData = np.unique(a['query'])
 entityList = fullEntityList
 
-#epochsPerWeek = int(6.048E5)
-#epochsPerDay = 86400
-#epochsPerInterval = epochsPerWeek
-#evalTR = 1325376000
-#evalTRend = 1338508800
 
 if DEBUG: entityList = testEntityList
 
-#starts = range(evalTR, evalTRend, epochsPerInterval)
-#intervalList = [(start, start + epochsPerInterval) for start in starts]
-#intervalList = [(1325379600,1328058000)]#, (0, 1371517333)]
 
 
 sliceBy = ('day' if epochsPerDay == epochsPerInterval else 'week' if epochsPerWeek == epochsPerInterval else 'other')
-
-#j= read_judgments(os.path.expanduser('~/kba-evaluation/taia/data/trec-kba-ccr-2012-judgments-2012JUN22-final.filter-run.txt'))
-#judgments = { (entity, intervalLow): j[np.logical_and(j['query'] == entity, 
-#                     np.logical_and(j['time'] >= intervalLow, j['time'] < intervalUp))]
-#          for entity in entityList
-#          for (intervalLow, intervalUp) in intervalList
-#        } 
-
-#resultsPerIntervall = [{} for interval in intervalList]
 
 
 records = []
@@ -164,32 +147,25 @@ for entity in entityList:
     for (i, (intervalLow, intervalUp)) in enumerate(intervalList):
         # segment data 
         all = a[a['query'] == entity]
-        #slice = a[np.logical_and(a['query'] == entity, a['time'] >= intervalLow, a['time'] < intervalUp)]
         slice = all[np.logical_and(all['time'] >= intervalLow, all['time'] < intervalUp)]
         if(len(slice)>0):
             # sort by confidence and revert (highest first)        
           slice = np.sort(slice, order='confidence')[::-1]
 
 
-        #groundtruth = j[np.logical_and(j['query'] == entity, np.logical_and(j['time'] >= intervalLow, j['time'] < intervalUp))]
-        #posGroundTruth = groundtruth[groundtruth['label']>=judgmentLevel]
         posGroundTruth = trueDocs(judgmentLevel, entity, intervalLow, intervalUp)
         
         numPos = len(posGroundTruth)
-        #numNeg = len(groundtruth) - numPos
         if DEBUG: print entity, i, judgmentLevel, 'pos:', numPos, 'neg:', numNeg, 'data:',len(slice), 'alldata', len(all)
         judgedPosSlice = np.array([np.count_nonzero(posGroundTruth['docid']==elem['docid']) > 0 for elem in slice])
         
         if DUMP_TREC_EVAL:
             for i,row in enumerate(slice):
-                #score = np.log2(1.0/(i+2.0))
                 score = float(row['confidence'])
                 rank = i+1
                 runOutputFile.write("%s\t%s\t%s\t%d\t%f\t%s\n"%(entity, intervalType, row['docid'],rank,score,row['runname']))
 
         
-        #unjudgedSlice = np.array([np.count_nonzero(groundtruth['docid']==elem['docid']) == 0 for elem in slice])
-        ##unjudgedAsPosSlice = np.logical_or(unjudgedSlice, judgedPosSlice)
         unjudgedAsNegSlice = judgedPosSlice
         numNeg = len(slice) - numPos        
         
@@ -209,22 +185,14 @@ for entity in entityList:
                                         numPosIntervals(judgmentLevel, entity, intervalType)))
         
          
-        #records.append(createEvalRecord(entity, intervalLow, intervalUp, '', judgmentLevel, 'numUnjudgedPredictions', sum(unjudgedSlice)))
         if numPos>0:
             if len(slice)>0:
                 for metricname, metric in metrics.items():
                     score = metric(unjudgedAsNegSlice, numPos, numNeg)
-                    #x = np.array([(team, runname, entity, intervalLow, intervalUp, 'neg',metricname, score)],  dtype=eval_dtype)            
                     records.append(createEvalRecord(entity, intervalLow, intervalUp, 'neg', judgmentLevel, metricname, score))
             else :
                 for metricname in metrics:
                     records.append(createEvalRecord(entity, intervalLow, intervalUp, 'neg', judgmentLevel, metricname, 0.0))
-
-# needs fixing, numPos is wrong
-#            for metricname, metric in metrics.items():
-#                score = metric(unjudgedAsPosSlice, numPos, numNeg)
-#                x = np.array([(entity, intervalLow, intervalUp, 'pos',metricname, score)],  dtype=eval_dtype)            
-#                records.append(x)
 
 print runFile
 
@@ -236,7 +204,6 @@ evalFile="%s-eval-%s.tsv"%(os.path.expanduser(runFile),intervalType)
 np.savetxt(evalFile, df,fmt='%s\t%s\t%s\t%d\t%d\t%s\t%d\t%s\t%f')
 
 
-#ndcgRAll = df[np.logical_and(df['unjudged']=='neg', df['metric']=='nDCG@R')]['value']
 print runFile
 
 for entity in entityList:
