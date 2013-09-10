00a8edaf67c3bccc429f202c0314b0ec22fcbead
Laura Dietz <dietz@cs.umass.edu>
2013-09-10 16:19:53 -0400

    Removing y2 entities without judgments from the list of evaluation entities

--- code/targetentities.py
+++ code/targetentities.py
@@ -52,7 +52,43 @@ fullEntityListYear1 = [
     'William_Cohen',
     'William_D._Cohan',
     'William_H._Gates,_Sr',
-    ]
+]
+
+unjudgedEntityListYear2 = [
+    'http://en.wikipedia.org/wiki/Stuart_Powell_Field'
+    , 'http://en.wikipedia.org/wiki/Corn_Belt_Power_Cooperative'
+    , 'http://en.wikipedia.org/wiki/Fargo-Moorhead_Symphony_Orchestra'
+    , 'http://en.wikipedia.org/wiki/John_D._Odegard_School_of_Aerospace_Sciences'
+    , 'http://en.wikipedia.org/wiki/Star_Lite_Motel'
+    , 'http://en.wikipedia.org/wiki/Elysian_Charter_School'
+    , 'http://en.wikipedia.org/wiki/Stevens_Cooperative_School'
+    , 'http://en.wikipedia.org/wiki/Lewis_and_Clark_Landing'
+    , 'http://en.wikipedia.org/wiki/Appleton_Museum_of_Art'
+    , 'http://en.wikipedia.org/wiki/Don_Garlits_Museum_of_Drag_Racing'
+    , 'http://en.wikipedia.org/wiki/Eighth_Street_Elementary_School'
+    , 'http://en.wikipedia.org/wiki/Marion_Technical_Institute'
+    , 'http://en.wikipedia.org/wiki/The_Ritz_Apartment_(Ocala,_Florida)'
+    , 'http://en.wikipedia.org/wiki/Great_American_Brass_Band_Festival'
+    , 'http://en.wikipedia.org/wiki/Hoboken_Volunteer_Ambulance_Corps'
+    , 'http://en.wikipedia.org/wiki/Agroindustrial_Pomalca'
+    , 'http://en.wikipedia.org/wiki/Intergroup_Financial_Services'
+    , 'https://twitter.com/GandBcoffee'
+    , 'https://twitter.com/evvnt'
+    , 'https://twitter.com/AlexJoHamilton'
+    , 'https://twitter.com/danvillekyengr'
+    , 'http://en.wikipedia.org/wiki/Carey_McWilliams_(marksman)'
+    , 'http://en.wikipedia.org/wiki/Fargo_Moorhead_Derby_Girls'
+    , 'http://en.wikipedia.org/wiki/Ken_Fowler'
+    , 'http://en.wikipedia.org/wiki/Chiara_Nappi'
+    , 'http://en.wikipedia.org/wiki/Henry_Gutierrez'
+    , 'http://en.wikipedia.org/wiki/Klaus_Grutzka'
+    , 'http://en.wikipedia.org/wiki/William_H._Miller_(writer)'
+    , 'http://en.wikipedia.org/wiki/Chuck_Pankow'
+    , 'http://en.wikipedia.org/wiki/Lorenzo_Williams_(basketball)'
+    , 'https://twitter.com/redmondmusic'
+    , 'https://twitter.com/MissMarcel'
+    , 'http://en.wikipedia.org/wiki/Fernando_J._Corbat%C3%B3'
+]
 
 
 def loadEntities():
@@ -65,6 +101,14 @@ def loadEntities():
 
     ## init our toy algorithm
     allentities = [rec['target_id'] for rec in filter_topics["targets"]]
+
     # filter out year 1 entities
     entities = [entity for entity in allentities if not any(map(entity.endswith, fullEntityListYear1))]
-    return entities
+    #return entities # uncomment this line, if you want to include entities without judgments.
+
+    # filter out entities without any positive judgments
+    judgedEntities = [entity for entity in entities if not entity in unjudgedEntityListYear2]
+    return judgedEntities
+
+
+
